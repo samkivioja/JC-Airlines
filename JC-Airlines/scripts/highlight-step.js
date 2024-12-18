825b94c468270bce6d63ca75481b69c2bb1f52c5
SanteriKivioja <70519088+samkivioja@users.noreply.github.com>
2024-12-18 10:47:09 +0200

    Update highlight-step.js

--- JC-Airlines/scripts/highlight-step.js
+++ JC-Airlines/scripts/highlight-step.js
@@ -1,11 +1,21 @@
 // Function to highlight the current step
-function highlightStep(stepIndex) {
-    const steps = document.querySelectorAll('.booking-steps p');
-    steps.forEach(step => step.classList.remove('highlighted'));
-    // Check if the element exists
-    if (steps[stepIndex]) {
-        steps[stepIndex].classList.add('highlighted'); // Highlight the step
-    }
+function highlightStep(currentStep) {
+    const steps = document.querySelectorAll('.booking-steps a');
+    steps.forEach(step => {
+        const stepIndex = parseInt(step.getAttribute('data-step'));
+
+        step.classList.remove('highlighted');
+
+        if (stepIndex === currentStep) {
+            step.querySelector('p').classList.add('highlighted'); // Highlight current step
+        }
+
+        if (stepIndex >= currentStep) {
+            step.style.pointerEvents = 'none'; // Disable clicking
+        } else {
+            step.style.pointerEvents = 'auto'; // Enable clicking
+        }
+    });
 }
 
 // Check the current page and highlight the correct step
